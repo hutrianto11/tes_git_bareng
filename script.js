18fc2ee00a9376402934b13ae755c0788ad766b2
hutrianto11 <hutrianto11@gmail.com>
2021-11-23 20:46:13 +0700

    belajargitbareng

---
+++ script.js
@@ -0,0 +1,34 @@
+//alert
+// alert('hello');
+// alert('nama');
+// alert('saya');
+// alert('agus hutrianto');
+
+//promt
+// var nama = prompt('masukan nama:');
+// alert(nama);
+
+//confirm
+// var tes = confirm('kamu yakin??');
+// alert(tes);
+
+//pengulangan (while menggunakan confirm)
+// var ulang = true;
+// while(ulang){
+//     console.log('hello world');
+//     ulang = confirm('lagi?');
+// }
+
+//pengulangan (while menggunakan program)
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 10){
+//     console.log('hello world' + nilaiAwal + 'x');
+// nilaiAwal++;
+// }
+
+//latihan pengulangan
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 10){
+//     console.log('Angkot NO. ' + nilaiAwal + ' beroprasi dengan baik.');
+// nilaiAwal++;
+// }
